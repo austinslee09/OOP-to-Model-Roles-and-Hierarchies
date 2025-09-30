23cd134ef2d5dd3a314542e70b302df2dcbffdd4
Austin Lee <austinlee@usf.edu>
2025-09-30 09:35:08 -0400

    fist commit

---
+++ cc_6.js
@@ -0,0 +1,10 @@
+// created class system
+class employee {
+constructor(name,dept){
+    this.name=name;
+    this.department=dept;
+}
+describe(){
+    return '${this.name} is in the ${this.department}';
+}
+}
