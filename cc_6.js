e8d7a22c9fec05bf10a126248b4cc7d09a831797
Austin Lee <austinlee@usf.edu>
2025-10-01 16:58:14 -0400

    step 4

--- cc_6.js
+++ cc_6.js
@@ -7,4 +7,20 @@ constructor(name,dept){
 describe(){
     return '${this.name} is in the ${this.department}';
 }
+//described the name and the department of employee
+}
+class managers extends employee {
+    constructor(name,dept){
+    super(name);
+    super(dept);
+    this.teamSize=teamSize;
+}
+}
+
+//added team size to the manager title
+new employee()
+new managers() 
+
+class company{
+    constructor
 }
